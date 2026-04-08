c8c2698ab1ce353b41cd2da66c183dece7cc5dd2
gomisenzo04-arch <gomisenzo04@gmail.com>
2026-04-08 19:11:05 +0200

    chore: bump service worker cache to v64

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Mode hors-ligne complet pour les 4 pages
    ============================================ */
 
-var CACHE_NAME = 'aurum-v63';
+var CACHE_NAME = 'aurum-v64';
 
 console.log('[Aurum SW] v3 active');
 
