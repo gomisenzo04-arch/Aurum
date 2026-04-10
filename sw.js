52e68e0e3f8179a6ad18507b059ed8d3ddb8dc7e
gomisenzo04-arch <gomisenzo04@gmail.com>
2026-04-10 04:25:14 +0200

    chore: bump SW v100 to force cache invalidation

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Mode hors-ligne complet pour les 4 pages
    ============================================ */
 
-var CACHE_NAME = 'aurum-v90';
+var CACHE_NAME = 'aurum-v100';
 
 console.log('[Aurum SW] v3 active');
 
